59794afcb66c1d68b0eb8f0cc432995a06e9883f
aymenkadri <aymen.kadri@gmail.com>
2013-10-12 21:56:02 +0200

    restructuration du projet

--- src/com/myApplication/client/MyApplicationServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package com.myApplication.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.TextBox;
-
-public interface MyApplicationServiceAsync {
-    void verifyEmail(String msg, AsyncCallback<String> async);
-}
