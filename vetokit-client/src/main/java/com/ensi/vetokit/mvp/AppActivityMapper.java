14bdce1dbd8efaac3daf46368c20609b052a32d7
aymenkadri <aymen.kadri@gmail.com>
2013-11-25 20:09:08 +0100

    Ajout d'un profil maven "draft" + Creation d'un fichier module dev

--- vetokit-client/src/main/java/com/ensi/vetokit/mvp/AppActivityMapper.java
+++ vetokit-client/src/main/java/com/ensi/vetokit/mvp/AppActivityMapper.java
@@ -3,8 +3,10 @@ package com.ensi.vetokit.mvp;
 
 import com.ensi.vetokit.activity.ClientActivity;
 import com.ensi.vetokit.activity.HomeActivity;
+import com.ensi.vetokit.activity.OnsActivity;
 import com.ensi.vetokit.place.ClientPlace;
 import com.ensi.vetokit.place.HomePlace;
+import com.ensi.vetokit.place.OnsPlace;
 import com.google.gwt.activity.shared.Activity;
 import com.google.gwt.activity.shared.ActivityMapper;
 import com.google.gwt.place.shared.Place;
@@ -19,12 +21,18 @@ public class AppActivityMapper implements ActivityMapper {
     @Inject
     Provider<ClientActivity> clientActivityProvider;
 
+    @Inject
+    Provider<OnsActivity> onsActivityProvider;
+
     public Activity getActivity(final Place place) {
         if (place instanceof HomePlace) {
             return homeActivityProvider.get().setPlace(place);
         }
         if (place instanceof ClientPlace) {
             return clientActivityProvider.get().setPlace(place);
+        }
+        if (place instanceof OnsPlace) {
+            return onsActivityProvider.get().setPlace(place);
         }
         return null;
     }
